d573c4219e1769ddbe1c59bc49fde1caae26d44b
radandbougie <mcgheekandice@gmail.com>
2018-11-02 20:33:13 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-// app.js
+//step one
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
+// step two
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//step three
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+//step four
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
 })
