a4e6e050c5f8a94b814fd464a2837aabd8b0b871
radandbougie <mcgheekandice@gmail.com>
2018-11-02 19:39:38 -0700

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('home', { msg: 'Handlebars are Cool!' });
+})
