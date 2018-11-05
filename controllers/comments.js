fe88cd1fec8ce0891733dfa74fd39495656433f4
radandbougie <mcgheekandice@gmail.com>
2018-11-05 00:15:11 -0800

    Users can delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,5 +6,14 @@ module.exports = (app) => {
   app.post('/reviews/comments', (req, res) => {
     res.send('reviews comment')
   })
+  // DELETE
+app.delete('/reviews/comments/:id', function (req, res) {
+  console.log("DELETE comment")
+  Comment.findByIdAndRemove(req.params.id).then((comment) => {
+    res.redirect(`/reviews/${comment.reviewId}`);
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 }
