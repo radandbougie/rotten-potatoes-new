eec87882316457764632e5a8e4b9e2ba53898ea0
radandbougie <mcgheekandice@gmail.com>
2018-11-04 23:06:53 -0800

    Users can see single reviews

--- app.js
+++ app.js
@@ -27,9 +27,13 @@ app.get('/', (req, res) => {
 })
 
 // SHOW
-app.get('/posts/:id', (req, res) => {
-
-});
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 // NEW
 app.get('/reviews/new', (req, res) => {
@@ -39,10 +43,10 @@ app.get('/reviews/new', (req, res) => {
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
   }).catch((err) => {
-    console.log(err.message);
+    console.log(err.message)
   })
 })
 
